caf549c30fe09759fb8745e32124f10c66eb9e50
aadhil <aadhil8903@gmail.com>
2021-06-03 13:24:42 +0530

    initial commit

--- src/Topnav.js
+++ src/Topnav.js
@@ -176,7 +176,7 @@ export default function App() {
   <div class="container p-4">
     <div class="row">
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase" class="text-info">pair-coders</h5>
+        <h5 class="text-uppercase text-light">pair-coders</h5>
 
         <p class="text-light">
           please dont hurt any of your pair
